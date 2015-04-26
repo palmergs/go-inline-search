68992038e0f468efae5309a168a9d8d16d9456ee
Galen Palmer <palmergs@gmail.com>
2015-04-25 22:06:40 -0400

    some utf-8 rune tests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "unicode/utf8"
 )
 
 type TokenNode struct {
@@ -20,6 +21,22 @@ func main() {
     fmt.Println("node = øoæ…œ∑áéøπö¥†®˚¬∆Ω≈ç√ñµ≤≥÷≠–ºª", tn)
     arr := []byte("node = øoæ…œ∑áéøπö¥†®˚¬∆Ω≈ç√ñµ≤≥÷≠–ºª")
     for i := range arr {
-        fmt.Println(arr[i])
+        fmt.Printf(" %x", arr[i])
+    }
+    fmt.Println("")
+    const nihongo = "日本語"
+    for index, runeValue := range nihongo {
+        fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+    }
+
+    for i, w := 0, 0; i < len(nihongo); i += w {
+        runeValue, width := utf8.DecodeRuneInString(nihongo[i:])
+        fmt.Printf("%#U starts at byte position %d\n", runeValue, i)
+        w = width
+    }
+
+    str := "日本語"
+    for index, runeValue := range str {
+        fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
     }
 }
