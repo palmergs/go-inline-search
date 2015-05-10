adf3b74cb9c15090f12b3713d26c024072536d79
Galen Palmer <palmergs@gmail.com>
2015-05-10 15:42:46 -0400

    clean up normalization code

--- token.go
+++ token.go
@@ -29,40 +29,44 @@ func (match *Token) Key() string {
 
 func NormalizeString(str string) string {
 	normalizedStr := make([]rune, 0)
-	whitespace := 0
-	charSeen := false
+	lastWasChar := true
+	charCount := 0
 	for _, runeValue := range str {
-		newRune, isChar := mapRuneWithState(runeValue)
-		if isChar {
-			if whitespace > 0 {
-				if charSeen {
-					normalizedStr = append(normalizedStr, ' ')
-				}
-				whitespace = 0
+		newRune, currIsChar := NormalizeRune(runeValue)
+		if currIsChar {
+			if !lastWasChar && charCount > 0 {
+				normalizedStr = append(normalizedStr, ' ')
 			}
 			normalizedStr = append(normalizedStr, newRune)
-			charSeen = true
-		} else {
-			whitespace = whitespace + 1
+			charCount++
 		}
+		lastWasChar = currIsChar
 	}
 	return string(normalizedStr)
 }
 
-func NormalizeRune(rn rune) rune {
-	newRune, _ := mapRuneWithState(rn)
-	return newRune
-}
-
-func mapRuneWithState(rn rune) (rune, bool) {
+func NormalizeRune(rn rune) (rune, bool) {
 	if unicode.IsPrint(rn) {
+
+		// whitespace, dashes and underscores are normalized to a single space
 		if unicode.IsSpace(rn) || rn == '-' || rn == '_' {
 			return ' ', false
 		}
+
+		// letters are normalized to lowercase
 		if unicode.IsLetter(rn) {
 			return unicode.ToLower(rn), true
 		}
-		return rn, true
+
+		// digits are passed through without modification
+		if unicode.IsDigit(rn) {
+			return rn, true
+		}
+
+		// return this rune as a character, but set state so there's no string of
+		return rn, false
 	}
+
+	// a non-printing character is returned as a space with non-character state set
 	return ' ', false
 }
