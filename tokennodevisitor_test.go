d30e490de52210f6d273c49a140fcfceec591049
Galen Palmer <palmergs@gmail.com>
2015-05-12 21:34:02 -0400

    add tests to verify additional document cases

--- tokennodevisitor_test.go
+++ tokennodevisitor_test.go
@@ -35,6 +35,31 @@ func TestAdvance(t *testing.T) {
 
 	root := buildTree()
 
+	success := searchDocument(root, `Learning ruby or Ruby on Rails, unlike pascal,
+			requires the programmer to learn rudamentary regular expressions. This is more
+			text that should be ignored. This is a test. Testing my brain. This is only a test!?`, 4)
+	if !success {
+		t.Errorf("Inline tokens not correctly found.\n")
+	}
+
+	success = searchDocument(root, `Searching for a token at end: ruby`, 1)
+	if !success {
+		t.Errorf("Couldn't find token at end of search string.\n")
+	}
+
+	success = searchDocument(root, `Ruby at start of document found?`, 1)
+	if !success {
+		t.Errorf("Couldn't find token at start of search string.\n")
+	}
+
+	success = searchDocument(root, `"ruby"`, 1)
+	if !success {
+		t.Errorf("Couldn't find token in quotes.\n")
+	}
+}
+
+func searchDocument(root *TokenNode, doc string, expected int) bool {
+
 	allMatches := make([]*TokenMatch, 0)
 	onMatch := func(matches []*TokenMatch) {
 		if matches != nil && len(matches) > 0 {
@@ -42,10 +67,8 @@ func TestAdvance(t *testing.T) {
 		}
 	}
 
+	document := NormalizeString(doc)
 	pool := NewTokenNodeVisitorPool(root)
-	document := NormalizeString(`Learning ruby or Ruby on Rails, unlike pascal,
-			requires the programmer to learn rudamentary regular expressions. This is more
-			text that should be ignored. This is a test. Testing my brain. This is only a test!?`)
 	currIsChar := false
 	lastWasChar := false
 	for i, w := 0, 0; i < len(document); i += w {
@@ -60,8 +83,7 @@ func TestAdvance(t *testing.T) {
 
 		pool.Advance(runeValue, i, onMatch)
 	}
+	pool.Advance('\n', 0, onMatch)
 
-	if len(allMatches) != 4 {
-		t.Errorf("Expected to find 4 matches in document but found %d :: %s", len(allMatches), allMatches)
-	}
+	return len(allMatches) == expected
 }
